732793d68c12acc8f7cbbf508e27d9467de859e1
Capsen Lu <capsen_lu@hotmail.com>
2022-08-13 22:40:59 +1000

    using pipe for validation

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -15,23 +15,29 @@ export class TasksService {
   getTasksWithFilter(filterDto: GetTasksFilterDto): Task[] {
     const { status, search } = filterDto;
     let tasks = this.getAllTasks();
-    if(status){
+    if (status) {
       tasks = tasks.filter((task) => task.status === status);
     }
 
-    if(search){
-      tasks = tasks.filter((task)=>{
-        if(task.title.includes(search) || task.description.includes(search)){
+    if (search) {
+      tasks = tasks.filter((task) => {
+        if (task.title.includes(search) || task.description.includes(search)) {
           return true;
         }
         return false;
-      })
+      });
     }
     return tasks;
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    // try to get task
+    const found = this.tasks.find((task) => task.id === id);
+    if (!found) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+
+    return found;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
@@ -52,11 +58,9 @@ export class TasksService {
     this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 
-  updateTaskStatus(id: string, status: TaskStatus){
+  updateTaskStatus(id: string, status: TaskStatus) {
     const task = this.getTaskById(id);
     task.status = status;
     return task;
   }
-
-
 }
